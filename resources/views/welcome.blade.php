f993dce7d140411889e0e14b47416f2037eb9ca6
RICHAR-PROG <2019141031@unh.edu.pe>
2023-04-24 16:18:40 -0500

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
+    <link rel="stylesheet" href="{{ asset('storage/css/style.css') }}">
+   
 
 </head>
 
@@ -48,10 +50,10 @@
                         <li class="">
                             <a href="{{ route('login') }}" class="p-4"> Login</a>
                         </li>
-                        <button type="button" class="btn " data-bs-toggle="modal" data-bs-target="#addprofile"
+                        <a type="button" class="btn " data-bs-toggle="modal" data-bs-target="#addprofile"
                             style="color: white">
                             Administrador
-                        </button>
+                    </a>
                     </ul>
                 </li>
             </ul>
