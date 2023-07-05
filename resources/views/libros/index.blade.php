592956112ac485bef114ca23c6e2d386be134f66
Micky02sn21 <mikel02sn@gmail.com>
2023-07-05 10:24:13 -0500

    cambio de tabla de libros

--- resources/views/libros/index.blade.php
+++ resources/views/libros/index.blade.php
@@ -6,12 +6,10 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap4.min.css">
 @endsection
 
-@section('content_header')
-    <h1>Lista de libros </h1>
-@stop
 
 @section('content')
-<a href="{{url('libros/create')}}" class="btn bg-gradient-info text-white my-2"> Registrar nuevo libro</a>
+{{--<a href="{{url('libros/create')}}" class="btn bg-gradient-info text-white my-2"> Registrar nuevo libro</a>
+
 <div class="row">
     <div class="card-body">
         <div class="table-responsive">
@@ -59,8 +57,73 @@
             </table>
         </div>
     </div>
-</div>
+</div>--}}
+<div class="container-fluid py-5">
+    <div class="card card-danger card-outline">
+        <div class="card-header">
+            <h3 class="card-title">Lista de libros</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-2 my-3">
 
+                    <a href="{{ url('libros/create') }}" class="btn btn-block btn-outline-primary "> <i class="fas fa-plus"></i> Registrar libro<i class="bi bi-person-fill-add"></i></a>
+
+                </div>
+
+                <div class="col-md-12 justify-content-center">
+                    <table
+                        class="table table-sm table-striped   table-full-width table-hover align-middle"
+                        id="libros">
+                        <thead class="bg-gradient-danger text-center">
+                            <tr>
+                                <td>Imagen</td>
+                                <td>Dewey</td>
+                                <td>Titulo</td>
+                                <td>Autor</td>
+                                <td>Editorial</td>
+                                <td>Estado</td>
+                                <td>Stock</td>
+                                <td>Formato</td>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($libros as $lib)
+                            <tr>
+                                <td>
+                                    <img src="{{ asset('storage/'.$lib->imagen) }}" alt="" width="100" height="100">                        </td>
+                                <td>{{ $lib->dewey }}</td>
+                                <td>{{ $lib->titulo }}</td>
+                                <td>{{ $lib->autor }}</td>
+                                <td>{{ $lib->editorial }}</td>
+                                <td>{{ $lib->estado }}</td>
+                                <td>{{ $lib->stock }}</td>
+                                <td>{{ $lib->formato }}</td>
+                                <td>
+                                    <div style="display: flex; align-items: center;" >
+                                        <a href="{{ url('/libros/'.$lib->id.'/edit')}}" class="btn bg-gradient-primary" style="margin-right: 10px;"><i class="fas fa-sync"></i>Editar</a>
+                                        <form action="{{url('/libros/'.$lib->id)}}" method="post">
+                                            @csrf
+                                            {{method_field('DELETE')}}
+                                            <button type="submit" onclick="return confirm('¿Quieres borrar?')" class="btn bg-gradient-warning "  > <i class='fas fa-trash'></i>Eliminar
+                                            </button>
+                                        </form>
+                                    </div>
+
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!-- /.card-body -->
+    </div>
+</div>
+</div>
 @stop
 
 @section('css')
